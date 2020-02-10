222423537c3fb117089980e7d6d58a671e94e758
AnshMittal1811 <anshm18111996@gmail.com>
2020-02-10 20:26:24 +0530

    Second Commit

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,16 @@
 # Building chatbot with D-NLP
 
+
+# Importing the Libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+
+
+############### PART 1 - DATA PREPROCESSING
+# Import Data
+m_lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+m_conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
