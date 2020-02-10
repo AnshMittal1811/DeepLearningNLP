882e921ebbaac912ddd30db727e8dd893b00ab9a
AnshMittal1811 <anshm18111996@gmail.com>
2020-02-10 20:57:26 +0530

    Third Commit

--- chatbot.py
+++ chatbot.py
@@ -14,3 +14,27 @@ import time
 # Import Data
 m_lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
 m_conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
+# Creating a Dictionary for mapping lines and id
+id2mline = {}
+for i in m_lines:
+    _i = i.split(' +++$+++ ')
+    if len(_i) == 5:
+        id2mline[_i[0]] = _i[4]
+        
+# Create list of all converstations
+conv_ids = []
+for j in m_conversations[:-1]:
+    _j = j.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    conv_ids.append(_j.split(','))
+
+# Separately Qustions and Answers
+que = []
+ans = []
+
+for k in conv_ids: 
+    for l in range(len(k) - 1):
+        que.append(id2mline[k[l]])
+        ans.append(id2mline[k[l+1]])
+        
+# Doing first cleaning of texts
